dcd82ceb1ad487fd0698be21fdf15afe9a05a5b7
abhishekmg <abhishekmg.12@gmail.com>
2022-09-06 16:31:40 +0530

    rendered edit and del for category owner too

--- pageComponents/dashboard/cardSection.tsx
+++ pageComponents/dashboard/cardSection.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { SingleListData } from '../../types/apiTypes';
+import { CategoriesData, SingleListData } from '../../types/apiTypes';
 import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid';
 import orderBy from 'lodash/orderBy';
 import { useRouter } from 'next/router';
-import { UNCATEGORIZED_URL } from '../../utils/constants';
-import { isEmpty } from 'lodash';
+import { CATEGORIES_KEY, UNCATEGORIZED_URL } from '../../utils/constants';
+import find from 'lodash/find';
+import isEmpty from 'lodash/isEmpty';
 import BookmarkCardSkeleton from '../../components/loadersSkeleton/bookmarkCardSkeleton';
 import Spinner from '../../components/spinner';
 import { useLoadersStore } from '../../store/componentStore';
 import Avatar from 'react-avatar';
+import { useQueryClient } from '@tanstack/react-query';
+import { PostgrestError } from '@supabase/supabase-js';
 
 interface CardSectionProps {
   listData: Array<SingleListData>;
@@ -30,6 +33,7 @@ const CardSection = ({
 }: CardSectionProps) => {
   const router = useRouter();
   const category_id = router?.asPath?.split('/')[1] || null;
+  const queryClient = useQueryClient();
 
   const isDeleteBookmarkLoading = useLoadersStore(
     (state) => state.isDeleteBookmarkLoading
@@ -40,6 +44,28 @@ const CardSection = ({
     category_id === UNCATEGORIZED_URL
       ? listData?.filter((item) => item?.category_id === null)
       : listData;
+
+  const categoryData = queryClient.getQueryData([CATEGORIES_KEY, userId]) as {
+    data: CategoriesData[];
+    error: PostgrestError;
+  };
+
+  const isLoggedInUserTheCategoryOwner =
+    find(categoryData?.data, (item) => item?.category_slug === category_id)
+      ?.user_id?.id === userId;
+
+  const renderEditAndDeleteCondition = (post: SingleListData) => {
+    if (isLoggedInUserTheCategoryOwner) {
+      return true;
+    } else {
+      // show if bookmark is created by loggedin user
+      if (post?.user_id === userId) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
 
   return (
     <div className="relative pb-20 lg:pb-28">
@@ -101,8 +127,7 @@ const CardSection = ({
                               className="mr-1"
                             />
                           )}
-                          {/* only show edit and del if logged in user is the owner of bookmarks */}
-                          {post?.user_id === userId && (
+                          {renderEditAndDeleteCondition(post) && (
                             <>
                               <PencilAltIcon
                                 className="h-5 w-5 text-gray-400 cursor-pointer"
