c32c04b1d835f851c835cb23f6d4f5c7a4609e1a
abhishekmg <abhishekmg.12@gmail.com>
2022-10-03 12:14:53 +0530

    added vercel.json for cros

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,12 +10,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: 'test',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
+  // await NextCors(req, res, {
+  //   // Options
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   origin: 'test',
+  //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  // });
 
   res.status(200).json({ name: 'John Doe' });
 }
