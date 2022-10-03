48a381f07fef49873cc7b7d8142018bbd3004775
abhishekmg <abhishekmg.12@gmail.com>
2022-10-03 12:20:24 +0530

    added test button for api testing cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import type { NextPage } from 'next';
 import Dashboard from '../pageComponents/dashboard';
 
 const Home: NextPage = () => {
   return (
     <>
+      <button
+        onClick={async () => {
+          const res = await axios.get(
+            'https://bookmark-tags-jbbi5sx2k-timelessco.vercel.app/api/hello'
+          );
+
+          console.log('ddd', res);
+        }}
+      >
+        test
+      </button>
       <Dashboard />
     </>
   );
