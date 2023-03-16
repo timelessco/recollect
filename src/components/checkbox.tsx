824de3f58e90322d57c5643f60c9f674170c6c6d
abhishekmg <abhishekmg.12@gmail.com>
2023-03-16 19:58:30 +0530

    style(batch actions): 🎨 added checkbox styles

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -14,16 +14,18 @@ const Checkbox = (props: CheckboxPropsTypes) => {
   return (
     // disabling this because as per docs htmlFor is not needed
     // eslint-disable-next-line jsx-a11y/label-has-associated-control
-    <label className="flex items-center">
+    <label className="checkbox-container relative flex items-center">
       <AriaCheckbox
         // className="h-4 w-4 rounded border-gray-300 text-custom-gray-5  transition-all duration-200 ease-in-out  disabled:opacity-50"
         onChange={e => onChange(e.target.value)}
         value={value}
         checked={checked}
         disabled={disabled}
+        className="aria-checkbox opacity-0"
       />
+      <div className="checkbox-div pointer-events-none absolute h-4 w-4" />
       <span
-        className={`ml-3 text-sm leading-[21px] text-custom-gray-5 ${
+        className={`checkmark ml-3 text-sm leading-[21px] text-custom-gray-5 ${
           disabled ? "opacity-50" : ""
         }`}
       >
