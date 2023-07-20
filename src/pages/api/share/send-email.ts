43a5067f4006bd9105dbaa161e4ec215acfd94bf
abhishekmg <abhishekmg.12@gmail.com>
2023-07-20 13:02:36 +0530

    feat(email invite): 🚧 added logs to debug send email errors

--- src/pages/api/share/send-email.ts
+++ src/pages/api/share/send-email.ts
@@ -8,12 +8,16 @@ import Email from "vercel-email";
 export default async function handler(request: NextRequest) {
 	const data = await request.json();
 
-	await Email.send({
+	console.warn("dddd", data.emailList, data.url);
+
+	const response: unknown = await Email.send({
 		to: data.emailList,
 		from: "noreply@laterpad.tmls.dev",
 		subject: "Laterpad Invite",
-		text: `Please click on this invite link to join the category ${data?.url}`,
+		text: `Please click on this invite link to join the category ${data.url}`,
 	});
+
+	console.warn("res", response);
 }
 
 export const config = {
