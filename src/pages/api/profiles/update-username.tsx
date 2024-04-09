7dece79f7d2d16e91050ada066e33922b837414e
abhishekmg <abhishekmg.12@gmail.com>
2024-04-09 10:56:55 +0530

    fix(405): 🚧 added logs

--- src/pages/api/profiles/update-username.tsx
+++ src/pages/api/profiles/update-username.tsx
@@ -32,65 +32,66 @@ export default async function handler(
 	request: NextApiRequest<UpdateUsernameApiPayload>,
 	response: NextApiResponse<Data>,
 ) {
-	if (request.method === "POST") {
-		const { error: _error } = verifyAuthToken(request.body.access_token);
+	const { error: _error } = verifyAuthToken(request.body.access_token);
 
-		if (_error) {
-			response.status(500).json({ data: null, error: _error });
-			throw new Error("ERROR: token error");
-		}
+	if (_error) {
+		response.status(500).json({ data: null, error: _error });
+		throw new Error("ERROR: token error");
+	}
 
-		const supabase = apiSupabaseClient();
+	const supabase = apiSupabaseClient();
 
-		// check if username is already presen
-		const {
-			data: checkData,
+	// check if username is already presen
+	const {
+		data: checkData,
+		error: checkError,
+	}: { data: DataResponse; error: ErrorResponse } = await supabase
+		.from(PROFILES)
+		.select(`user_name`)
+		.eq("user_name", request.body.username);
+
+	if (!isNull(checkError)) {
+		response.status(500).json({
+			data: null,
 			error: checkError,
+		});
+		throw new Error("ERROR: check username DB error");
+	}
+
+	if (isEmpty(checkData)) {
+		// user name is not there so we update
+		const {
+			data: updateData,
+			error: updateError,
 		}: { data: DataResponse; error: ErrorResponse } = await supabase
 			.from(PROFILES)
-			.select(`user_name`)
-			.eq("user_name", request.body.username);
+			.update({
+				user_name: request.body.username,
+			})
+			.match({ id: request.body.id })
+			.select(`user_name`);
 
-		if (!isNull(checkError)) {
+		if (!isNull(updateError)) {
 			response.status(500).json({
 				data: null,
-				error: checkError,
-			});
-			throw new Error("ERROR: check username DB error");
-		}
-
-		if (isEmpty(checkData)) {
-			// user name is not there so we update
-			const {
-				data: updateData,
 				error: updateError,
-			}: { data: DataResponse; error: ErrorResponse } = await supabase
-				.from(PROFILES)
-				.update({
-					user_name: request.body.username,
-				})
-				.match({ id: request.body.id })
-				.select(`user_name`);
-
-			if (!isNull(updateError)) {
-				response.status(500).json({
-					data: null,
-					error: updateError,
-				});
-				throw new Error("ERROR: update username in db error");
-			}
-
-			response.status(200).json({
-				data: updateData,
-				error: null,
 			});
-		} else {
-			// user name is already present in the DB
-			response.status(500).json({
-				data: null,
-				error: "Username already exists, please try another username",
-			});
-			// throw new Error("ERROR: username is already present");
+			throw new Error("ERROR: update username in db error");
 		}
+
+		response.status(200).json({
+			data: updateData,
+			error: null,
+		});
+	} else {
+		// user name is already present in the DB
+		response.status(500).json({
+			data: null,
+			error: "Username already exists, please try another username",
+		});
+		// throw new Error("ERROR: username is already present");
 	}
+
+	// eslint-disable-next-line no-console
+	console.log("OVER TESTING");
 }
