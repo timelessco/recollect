e291d8921806d5ecabd38eadfaf27fbf7d690e4c
abhishekmg <abhishekmg.12@gmail.com>
2024-04-16 16:37:07 +0530

    fix(error toast): 🐛 fixed unnessary error toast call

--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -1,5 +1,3 @@
-import isNull from "lodash/isNull";
-
 import { errorToast } from "./toastMessages";
 
 // This file has front end api related helpers
@@ -13,18 +11,6 @@ const errorTextLogic = (error: unknown) =>
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const mutationApiCall = async (apiCall: Promise<any>) => {
 	const response = await apiCall;
-
-	if (!isNull(response?.error)) {
-		errorToast(errorTextLogic(response?.error?.message));
-	}
-
-	if (!isNull(response?.data?.message)) {
-		errorToast(errorTextLogic(response?.data?.message));
-	}
-
-	if (response?.response?.data?.error?.message) {
-		errorToast(errorTextLogic(response?.response?.data?.error?.message));
-	}
 
 	if (response?.response?.status !== 200 && response?.response?.data?.error) {
 		errorToast(errorTextLogic(response?.response?.data?.error));
