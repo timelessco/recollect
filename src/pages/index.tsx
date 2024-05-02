7f0ac9e5609c794600b79b4000e0d2769bfe550a
abhishekmg <34393560+abhishekmg@users.noreply.github.com>
2024-05-02 16:28:09 +0530

    fix(ssr): 🐛 removed getserversideprops (#190)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,28 +5,28 @@ import Dashboard from "../pageComponents/dashboard";
 
 const Home: NextPage = () => <Dashboard />;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-	// Create authenticated Supabase Client
-	const supabase = createServerSupabaseClient(context);
-	// Check if we have a session
-	const {
-		data: { session },
-	} = await supabase.auth.getSession();
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+// 	// Create authenticated Supabase Client
+// 	const supabase = createServerSupabaseClient(context);
+// 	// Check if we have a session
+// 	const {
+// 		data: { session },
+// 	} = await supabase.auth.getSession();
 
-	if (!session)
-		return {
-			redirect: {
-				destination: "/login",
-				permanent: false,
-			},
-		};
+// 	if (!session)
+// 		return {
+// 			redirect: {
+// 				destination: "/login",
+// 				permanent: false,
+// 			},
+// 		};
 
-	return {
-		props: {
-			initialSession: session,
-			user: session.user,
-		},
-	};
-};
+// 	return {
+// 		props: {
+// 			initialSession: session,
+// 			user: session.user,
+// 		},
+// 	};
+// };
 
 export default Home;
