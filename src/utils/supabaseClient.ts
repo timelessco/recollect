2358944314a8d72a3ee362b5083b544a23801953
abhishekmg <abhishekmg.12@gmail.com>
2024-05-07 14:02:46 +0530

    fix: 🐛 made public page api work

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -12,6 +12,10 @@ const developmentSupabaseAnonKey = process.env.NEXT_PUBLIC_DEV_SUPABASE_ANON_KEY
 	? process.env.NEXT_PUBLIC_DEV_SUPABASE_ANON_KEY
 	: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+const developmentSupabaseServiceKey = process.env.DEV_SUPABASE_SERVICE_KEY
+	? process.env.DEV_SUPABASE_SERVICE_KEY
+	: process.env.SUPABASE_SERVICE_KEY;
+
 export const supabaseUrl = !isProductionEnvironment
 	? developmentSupabaseUrl
 	: process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -20,8 +24,18 @@ export const supabaseAnonKey = !isProductionEnvironment
 	? developmentSupabaseAnonKey
 	: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+export const supabaseServiceKey = !isProductionEnvironment
+	? developmentSupabaseServiceKey
+	: process.env.SUPABASE_SERVICE_KEY;
+
 export const createClient = () => {
 	const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
+
+	return supabase;
+};
+
+export const createServiceClient = () => {
+	const supabase = createBrowserClient(supabaseUrl, supabaseServiceKey);
 
 	return supabase;
 };
