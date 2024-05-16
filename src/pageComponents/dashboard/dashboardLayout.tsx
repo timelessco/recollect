353f008a0c72a75bc4073dab98b91dbd292272e4
abhishekmg <abhishekmg.12@gmail.com>
2024-05-16 13:12:53 +0530

    fix(side pane): 🐛 cards section padding and side pane click open

--- src/pageComponents/dashboard/dashboardLayout.tsx
+++ src/pageComponents/dashboard/dashboardLayout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useCallback, useEffect, useRef, useState } from "react";
 import { type PostgrestError } from "@supabase/supabase-js";
 import { useQueryClient } from "@tanstack/react-query";
@@ -396,7 +398,6 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 				show: currentPath === TRASH_URL,
 				value: "trash",
 				render: (
-					// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
 					<div
 						className={`flex items-center text-red-700 ${dropdownMenuItemClassName}`}
 						onClick={() => onClearTrash()}
@@ -415,7 +416,6 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 				show: typeof categoryId === "number",
 				value: "rename",
 				render: (
-					// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
 					<div
 						className={`flex items-center ${dropdownMenuItemClassName}`}
 						onClick={() => {
@@ -446,7 +446,6 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 		const optionsList = optionsData
 			?.filter((optionItem) => optionItem?.show === true)
 			?.map((item) => (
-				// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
 				<div
 					key={item?.value}
 					onClick={() => updateHeaderOptionCurrentTab(item?.value)}
@@ -634,6 +633,16 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 
 	const renderDeskTopView = (
 		<div style={{ width: "100vw", height: "100vh" }}>
+			{!showSidePane && (
+				<div
+					className=" absolute left-0 z-[10] h-[100vh] w-5 cursor-pointer"
+					onClick={() => {
+						setShowSidePane(true);
+						// opens side pane
+						setTimeout(() => allotmentRef?.current?.reset(), 120);
+					}}
+				/>
+			)}
 			<Allotment
 				className="split-view-container"
 				onChange={(value: number[]) => {
