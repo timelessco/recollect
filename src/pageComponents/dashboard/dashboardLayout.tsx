f7091ceb91b5763e1edb689c1797b78887105440
abhishekmg <abhishekmg.12@gmail.com>
2024-05-16 13:28:43 +0530

    fix(side pane): 🐛 fixed side pane open on click logic

--- src/pageComponents/dashboard/dashboardLayout.tsx
+++ src/pageComponents/dashboard/dashboardLayout.tsx
@@ -155,7 +155,7 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 	const [showSearchBar, setShowSearchBar] = useState(true);
 
 	const allotmentRef = useRef<AllotmentHandle>(null);
-	const paneRef = useRef(null);
+	const paneRef = useRef<HTMLDivElement>(null);
 
 	// this is the resize pane animation logic
 	useEffect(() => {
@@ -633,7 +633,7 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 
 	const renderDeskTopView = (
 		<div style={{ width: "100vw", height: "100vh" }}>
-			{!showSidePane && (
+			{/* {!showSidePane && (
 				<div
 					className=" absolute left-0 z-[10] h-[100vh] w-5 cursor-pointer"
 					onClick={() => {
@@ -642,13 +642,10 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 						setTimeout(() => allotmentRef?.current?.reset(), 120);
 					}}
 				/>
-			)}
+			)} */}
 			<Allotment
 				className="split-view-container"
 				onChange={(value: number[]) => {
-					// console.log("vv", value[0]);
-
-					// setIsDraggingValue(value[0]);
 					if (value[0] === 0) {
 						setShowSidePane(false);
 					}
@@ -659,8 +656,15 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 				}}
 				onDragEnd={(values: number[]) => {
 					const leftPaneSize = values?.[0];
-					if (leftPaneSize < 180) {
-						// closes the side pane based on resize width
+					if (leftPaneSize === 0 && paneRef?.current?.clientWidth === 0) {
+						// open side pane when its fully closed and on the resize pane click
+						setShowSidePane(true);
+						// opens side pane
+						setTimeout(() => allotmentRef?.current?.reset(), 120);
+					}
+
+					if (leftPaneSize < 180 && paneRef?.current?.clientWidth > 0) {
+						// closes the side pane when user is resizing it and side pane is less than 180px
 						setTimeout(() => allotmentRef?.current?.resize([0, 100]), 100);
 						setShowSidePane(false);
 					}
