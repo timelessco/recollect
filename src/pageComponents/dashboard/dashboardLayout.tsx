68141cc272722ba11c6da77ee6ba67f14fac7be0
abhishekmg <34393560+abhishekmg@users.noreply.github.com>
2024-05-16 15:38:10 +0530

    fix(card section): 💄 added overall padding (#203)

--- src/pageComponents/dashboard/dashboardLayout.tsx
+++ src/pageComponents/dashboard/dashboardLayout.tsx
@@ -633,16 +633,6 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 
 	const renderDeskTopView = (
 		<div style={{ width: "100vw", height: "100vh" }}>
-			{/* {!showSidePane && (
-				<div
-					className=" absolute left-0 z-[10] h-[100vh] w-5 cursor-pointer"
-					onClick={() => {
-						setShowSidePane(true);
-						// opens side pane
-						setTimeout(() => allotmentRef?.current?.reset(), 120);
-					}}
-				/>
-			)} */}
 			<Allotment
 				className="split-view-container"
 				onChange={(value: number[]) => {
