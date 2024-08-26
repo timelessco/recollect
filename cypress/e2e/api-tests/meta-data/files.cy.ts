7c05c3907c7459ed1160f7dd062b70f36ae74fc7
abhishekmg <abhishekmg.12@gmail.com>
2024-08-26 11:35:49 +0530

    test(meta data): ✅ added all the meta data test cases

---
+++ cypress/e2e/api-tests/meta-data/files.cy.ts
@@ -0,0 +1,75 @@
+//@ts-nocheck
+
+describe("Meta data testing", () => {
+	beforeEach(() => {
+		// cy.visit("/all-bookmarks");
+		cy.visit("/login");
+	});
+
+	it("file upload check", async () => {
+		cy.wait(3000);
+
+		cy.login(
+			Cypress.env("test_email") as string,
+			Cypress.env("test_password") as string,
+		);
+
+		const headers = {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		};
+		cy.request({
+			method: "POST",
+			url: "/api/v1/tests/file/post/upload", // Ensure this URL is correct
+			body: {
+				name: "image.png",
+				type: "image/png",
+				uploadFileNamePath: "m05eqwhx-image.png",
+			},
+			headers: headers,
+		}).as("uploadRequest");
+
+		cy.get("@uploadRequest")?.then((addBookmarkData) => {
+			bookmarkId = addBookmarkData?.body?.data?.[0]?.id;
+
+			// check meta data
+
+			cy.reload();
+
+			cy.wait(60000);
+
+			cy.reload();
+
+			cy.wait(2000);
+
+			cy.request(`/api/v1/bookmarks/get/fetch-by-id`, {
+				data: {
+					id: addBookmarkData?.body?.data?.[0]?.id,
+				},
+			}).as("fetchRequest");
+
+			cy.get("@fetchRequest").then((data) => {
+				cy.wait(1000);
+
+				expect(data?.body?.data?.[0]?.meta_data).to.not.be.null;
+				expect(data?.body?.data?.[0]?.meta_data?.ocr).to.not.be.null;
+				expect(data?.body?.data?.[0]?.meta_data?.img_caption).to.not.be.null;
+				expect(data?.body?.data?.[0]?.meta_data?.ogImgBlurUrl).to.not.be.null;
+			});
+
+			// delete the bookmark
+
+			cy.request("DELETE", `/api/v1/bookmarks/delete/non-cascade`, {
+				data: {
+					id: addBookmarkData?.body?.data?.[0]?.id,
+				},
+			}).as("deleteRequest");
+
+			cy.get("@deleteRequest").then((data) => {
+				cy.wait(1000);
+
+				expect(data.status).to.eq(200);
+			});
+		});
+	});
+});
