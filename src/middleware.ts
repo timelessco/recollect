ed467a8a1b98373ae15ea5ed0c707b1480dcae04
widejoy <rogerantony40@gmail.com>
2025-06-16 12:14:02 +0530

    feat(middleware): ✨ enhance login redirection to include signup path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,19 @@
 // middleware.ts
 import { NextResponse, type NextRequest } from "next/server";
 
+import { ALL_BOOKMARKS_URL } from "./utils/constants";
+
 export const middleware = (request: NextRequest) => {
-	const token = request.cookies.get("sb-fgveraehgourpwwzlzhy-auth-token")
+	const token = request.cookies.get(process.env.AUTH_COOKIE_SECRET ?? "")
 		?.value;
 
 	// If logged in, redirect from /login to /all-bookmarks
-	if (token && request.nextUrl.pathname === "/login") {
-		return NextResponse.redirect(new URL("/all-bookmarks", request.url));
+	if (
+		token &&
+		(request.nextUrl.pathname === "/login" ||
+			request.nextUrl.pathname === "/signup")
+	) {
+		return NextResponse.redirect(new URL(`/${ALL_BOOKMARKS_URL}`, request.url));
 	}
 
 	return NextResponse.next();
