9580d4c0bed8af3ad5d00aafb4c6b01a09412e06
widejoy <rogerantony40@gmail.com>
2025-07-08 14:48:25 +0530

    refactor(bookmark-screenshot): update mutation hook to use new category and sort hooks

--- src/pageComponents/dashboard/cardSection/index.tsx
+++ src/pageComponents/dashboard/cardSection/index.tsx
@@ -552,19 +552,6 @@ const CardSection = ({
 						{img ? (
 							documentFileTypes?.includes(type) ? (
 								<PDFThumbnail className={imgClassName} pdfUrl={img} />
-							) : cardTypeCondition === viewValues.moodboard ? (
-								<div className="pointer-events-none relative aspect-[4/3] w-full">
-									<Image
-										alt="bookmark-img"
-										className={`${imgClassName} object-cover`}
-										fill
-										{...(blurSource
-											? { placeholder: "blur", blurDataURL: blurSource }
-											: { placeholder: "empty" })}
-										onError={() => setErrorImgs([id as never, ...errorImgs])}
-										src={img}
-									/>
-								</div>
 							) : (
 								<Image
 									alt="bookmark-img"
