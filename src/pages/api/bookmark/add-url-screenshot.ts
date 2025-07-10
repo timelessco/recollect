257ac0b3fa93791519a622152943d040ce1cb6b5
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-10 13:58:40 +0530

    fix(bookmark): remove debug logging and correct API endpoint for adding remaining bookmark data

--- src/pages/api/bookmark/add-url-screenshot.ts
+++ src/pages/api/bookmark/add-url-screenshot.ts
@@ -109,7 +109,6 @@ export default async function handler(
 		screenshot: publicURL,
 		coverImage: existingBookmarkData?.ogImage,
 	};
-	console.error("updatedMetaData", updatedMetaData);
 
 	const {
 		data,
@@ -130,8 +129,7 @@ export default async function handler(
 		try {
 			if (data && data.length > 0) {
 				await axios.post(
-					`https://bookmark-tags-git-fix-ogimgaes-debugging-timelessco.vercel.app/${NEXT_API_URL}${ADD_REMAINING_BOOKMARK_API}`,
-					// `${getBaseUrl()}${NEXT_API_URL}${ADD_REMAINING_BOOKMARK_API}`,
+					`${getBaseUrl()}${NEXT_API_URL}${ADD_REMAINING_BOOKMARK_API}`,
 					{
 						id: data[0]?.id,
 						favIcon: data?.[0]?.meta_data?.favIcon,
