7a30dafe00185f687f8f1220e1b5de2ac2863858
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-10 15:21:15 +0530

    fix: refactor URL handling to use getBaseUrl and remove unused variables

--- src/pages/api/bookmark/add-bookmark-min-data.ts
+++ src/pages/api/bookmark/add-bookmark-min-data.ts
@@ -25,12 +25,10 @@ import {
 	getBaseUrl,
 	MAIN_TABLE_NAME,
 	NEXT_API_URL,
-	OG_IMAGE_PREFERRED_SITES,
 	SHARED_CATEGORIES_TABLE_NAME,
 	uncategorizedPages,
-	URL_IMAGE_CHECK_PATTERN,
 } from "../../../utils/constants";
-import { apiCookieParser } from "../../../utils/helpers";
+import { apiCookieParser, checkIsUrlAnImage } from "../../../utils/helpers";
 import { apiSupabaseClient } from "../../../utils/supabaseServerClient";
 
 // this api get the scrapper data, checks for duplicate bookmarks and then adds it to the DB
@@ -108,18 +106,6 @@ export default async function handler(
 	const { url } = request.body;
 	const { category_id: categoryId } = request.body;
 	const { update_access: updateAccess } = request.body;
-
-	const urlHost = new URL(url).hostname.toLowerCase();
-	const urlString = url.toLowerCase();
-
-	const isOgImagePreferred = OG_IMAGE_PREFERRED_SITES.some(
-		(keyword) => urlHost.includes(keyword) || urlString.includes(keyword),
-	);
-
-	console.error(
-		"isOgImagePreferred in min data~~~~~~~~~~~~~~~~",
-		isOgImagePreferred,
-	);
 
 	// try {
 	// 	// 5 seconds timeout
@@ -295,9 +281,9 @@ export default async function handler(
 
 	let ogImageToBeAdded = null;
 
-	const isUrlOfMimeType = url?.match(URL_IMAGE_CHECK_PATTERN);
+	const isUrlAnImage = await checkIsUrlAnImage(url);
 
-	if (!isNil(isUrlOfMimeType)) {
+	if (isUrlAnImage) {
 		ogImageToBeAdded = url;
 	} else {
 		ogImageToBeAdded = scrapperResponse?.data?.OgImage;
@@ -343,10 +329,10 @@ export default async function handler(
 			.json({ data, error: scraperApiError ?? null, message: null });
 
 		try {
-			if (!isNull(data) && !isEmpty(data) && !isNil(isUrlOfMimeType)) {
+			if (!isNull(data) && !isEmpty(data) && !isNil(isUrlAnImage)) {
 				// this adds the remaining data , like blur hash bucket uploads and all
 				await axios.post(
-					`https://bookmark-tags-git-fix-ogimgaes-debugging-timelessco.vercel.app${NEXT_API_URL}${ADD_REMAINING_BOOKMARK_API}`,
+					`${getBaseUrl()}${NEXT_API_URL}${ADD_REMAINING_BOOKMARK_API}`,
 					{
 						id: data[0]?.id,
 						favIcon: scrapperResponse?.data?.favIcon,
