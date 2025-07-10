e78e24d7020bf618616df4d65579b1fa203a35db
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-10 14:41:19 +0530

    fix(bookmark): update ogImage assignment to use coverImage from meta_data

--- src/pages/api/bookmark/add-remaining-bookmark-data.ts
+++ src/pages/api/bookmark/add-remaining-bookmark-data.ts
@@ -312,7 +312,7 @@ export default async function handler(
 		.update({
 			meta_data,
 			ogImage: isOgImagePreferred
-				? ogImageMetaDataGeneration
+				? meta_data?.coverImage
 				: imageUrlForMetaDataGeneration,
 		})
 		.match({ id })
