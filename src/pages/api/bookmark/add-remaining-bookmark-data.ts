f5677b63a7c019848ac853714402861f5c6a590e
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-10 15:36:51 +0530

    fix(bookmark): update ogImage assignment logic for preferred sites and metadata generation

--- src/pages/api/bookmark/add-remaining-bookmark-data.ts
+++ src/pages/api/bookmark/add-remaining-bookmark-data.ts
@@ -230,9 +230,9 @@ export default async function handler(
 	// generat meta data (ocr, blurhash data, imgcaption)
 	const imageUrlForMetaDataGeneration = isUrlAnImageCondition
 		? uploadedImageThatIsAUrl
-		: !isNil(uploadedCoverImageUrl)
-		? uploadedCoverImageUrl
-		: currentData?.meta_data?.screenshot;
+		: !isNil(currentData?.meta_data?.screenshot)
+		? currentData?.meta_data?.screenshot
+		: uploadedCoverImageUrl;
 
 	if (
 		!isNil(imageUrlForMetaDataGeneration) &&
@@ -295,7 +295,9 @@ export default async function handler(
 		.from(MAIN_TABLE_NAME)
 		.update({
 			meta_data,
-			ogImage: imageUrlForMetaDataGeneration,
+			ogImage: isOgImagePreferred
+				? ogImageMetaDataGeneration
+				: imageUrlForMetaDataGeneration,
 		})
 		.match({ id })
 		.select(`id`);
