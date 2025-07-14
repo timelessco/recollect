79016f8645447efe2abe926f2980ba931feb4fe1
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-14 16:25:50 +0530

    fix: made changes in the condition  isUrlOfMimeType(min-data), currentData?.ogImage(rem-data)

--- src/pages/api/bookmark/add-bookmark-min-data.ts
+++ src/pages/api/bookmark/add-bookmark-min-data.ts
@@ -303,7 +303,7 @@ export default async function handler(
 	// ***** here we are checking the url is of an mime type or not,if it is so we set the url in ogImage *****
 	// ***** if it an  image we upload to s3 and for video we take screenshot *****
 
-	if (!isNil(isUrlOfMimeType)) {
+	if (isUrlOfMimeType) {
 		ogImageToBeAdded = url;
 	} else {
 		ogImageToBeAdded = scrapperResponse?.data?.OgImage;
@@ -351,7 +351,7 @@ export default async function handler(
 			.json({ data, error: scraperApiError ?? null, message: null });
 
 		try {
-			if (!isNull(data) && !isEmpty(data) && !isNil(isUrlOfMimeType)) {
+			if (!isNull(data) && !isEmpty(data) && !isUrlOfMimeType) {
 				// this adds the remaining data , like blur hash bucket uploads and all
 				// this is called only if the url is an image url like test.com/image.png.
 				// for other urls we call the screenshot api in the client side and in that api the remaining bookmark api (the one below is called)
