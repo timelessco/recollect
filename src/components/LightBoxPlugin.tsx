ad73109482ba5a610fd79911d74f4f4d10e245e9
widejoy <rogerantony40@gmail.com>
2025-07-25 17:22:57 +0530

    feat(sidepane): ✨ implemented sidepane using plugin

--- src/components/LightBoxPlugin.tsx
+++ src/components/LightBoxPlugin.tsx
@@ -1,30 +1,13 @@
 // plugins/MetaButtonPlugin.tsx
-import { useState } from "react";
-import { addToolbarButton, type Plugin } from "yet-another-react-lightbox";
+import { type Plugin } from "yet-another-react-lightbox";
 
 export default function MetaButtonPlugin(): Plugin {
-	const [showSidepane, setShowSidepane] = useState(false);
-	return ({ addSibling, augment }) => {
+	return ({ addSibling }) => {
 		addSibling("toolbar", {
 			name: "meta-button",
 			component: () => (
-				<div className="absolute bottom-auto left-auto right-0 top-0 flex w-[200px] justify-end bg-red-950 " />
+				<div className="absolute bottom-auto left-auto right-0 top-0 flex h-[200px] w-[200px] justify-end bg-red-950 " />
 			),
 		});
-		augment(({ toolbar, ...restProps }) => ({
-			toolbar: addToolbarButton(
-				toolbar,
-				"Meta",
-				<button
-					className=" text-gray-500 transition hover:text-gray-700"
-					key="show-pane"
-					onClick={() => setShowSidepane(true)}
-					type="button"
-				>
-					Show Meta Data
-				</button>,
-			),
-			...restProps,
-		}));
 	};
 }
