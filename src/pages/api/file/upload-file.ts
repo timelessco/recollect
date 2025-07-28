f76c09f5a3bd3ec8051eaa7bd2f3325ba98f96a1
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-28 12:46:34 +0530

    fix: add method:GET  in upload file

--- src/pages/api/file/upload-file.ts
+++ src/pages/api/file/upload-file.ts
@@ -207,6 +207,7 @@ export default async (
 		// 	await notVideoLogic(storageData);
 
 		const responsePdf = await fetch(storageData?.publicUrl, {
+			method: "GET",
 			headers: {
 				"User-Agent":
 					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
