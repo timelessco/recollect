034545c5d6371fe72793304ab3fcef57e558a33a
B-a-l-aj-i <balajivijay679@gmail.com>
2025-07-28 14:15:37 +0530

    fix: add  referer

--- src/pages/api/file/upload-file.ts
+++ src/pages/api/file/upload-file.ts
@@ -211,6 +211,9 @@ export default async (
 			headers: {
 				"User-Agent":
 					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+				Accept: "*/*",
+				Referer:
+					"https://bookmark-tags-git-feat-local-pdf-thumbnail-timelessco.vercel.app",
 			},
 		});
 
