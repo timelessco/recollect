0c45326d195070f380f399cb1bc473d3338bff1b
widejoy <rogerantony40@gmail.com>
2025-08-08 14:49:23 +0530

    chore(comments): 📝 added omment

--- src/components/lightbox/LightBox.tsx
+++ src/components/lightbox/LightBox.tsx
@@ -267,6 +267,7 @@ export const CustomLightBox = ({
 				content = renderWebEmbedSlide();
 			}
 
+			// content wrapper
 			return (
 				<div className="flex h-full w-full items-center justify-center">
 					{content}
