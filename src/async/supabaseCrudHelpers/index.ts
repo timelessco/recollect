14e7b3342b4e5f98f9d828f4bf22fa657bd9e7b8
Balaji <balajivijay679@gmail.com>
2025-10-30 11:10:00 +0530

    feat(verify-email): signIn and signUp using OTP (#523)

--- src/async/supabaseCrudHelpers/index.ts
+++ src/async/supabaseCrudHelpers/index.ts
@@ -47,6 +47,7 @@ import {
 	ADD_CATEGORY_TO_BOOKMARK_API,
 	ADD_TAG_TO_BOOKMARK_API,
 	ADD_URL_SCREENSHOT_API,
+	ALL_BOOKMARKS_URL,
 	CLEAR_BOOKMARK_TRASH_API,
 	CREATE_USER_CATEGORIES_API,
 	CREATE_USER_TAGS_API,
@@ -855,15 +856,34 @@ export const signInWithOauth = async (
 	await supabase.auth.signInWithOAuth({ provider });
 };
 
-export const signInWithEmailPassword = async (
+export const signInWithOtp = async (
 	email: string,
-	password: string,
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	supabase: SupabaseClient<any, "public", any>,
 ) => {
-	const { error } = await supabase.auth.signInWithPassword({ email, password });
+	const { data, error } = await supabase.auth.signInWithOtp({
+		email,
+		options: {
+			shouldCreateUser: true,
+			emailRedirectTo: `${getBaseUrl()}/${ALL_BOOKMARKS_URL}`,
+		},
+	});
 
-	return { error };
+	return { data, error };
+};
+
+export const verifyOtp = async (
+	email: string,
+	otp: string,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	supabase: SupabaseClient<any, "public", any>,
+) => {
+	const { data, error } = await supabase.auth.verifyOtp({
+		email,
+		token: otp,
+		type: "email",
+	});
+	return { data, error };
 };
 
 export const signUpWithEmailPassword = async (
--- src/pageComponents/login/index.tsx
+++ src/pageComponents/login/index.tsx
@@ -6,8 +6,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import {
-	signInWithEmailPassword,
 	signInWithOauth,
+	signInWithOtp,
+	verifyOtp,
 } from "../../async/supabaseCrudHelpers";
 import Input from "../../components/atoms/input";
 import Spinner from "../../components/spinner";
@@ -15,22 +16,20 @@ import GoogleLoginIcon from "../../icons/googleLoginIcon";
 import LaterpadLogo from "../../icons/laterpadLogo";
 import { useSupabaseSession } from "../../store/componentStore";
 import {
-	bottomBarButton,
-	bottomBarText,
 	buttonDarkClassName,
 	buttonLightClassName,
 	grayInputClassName,
 } from "../../utils/commonClassNames";
-import {
-	ALL_BOOKMARKS_URL,
-	EMAIL_CHECK_PATTERN,
-	SIGNUP_URL,
-} from "../../utils/constants";
+import { ALL_BOOKMARKS_URL, EMAIL_CHECK_PATTERN } from "../../utils/constants";
 import { createClient } from "../../utils/supabaseClient";
 import { errorToast } from "../../utils/toastMessages";
 
 const LoginPage = () => {
 	const [isLoading, setIsLoading] = useState(false);
+	const [isOtpStep, setIsOtpStep] = useState(false);
+	const [email, setEmail] = useState("");
+	const [otp, setOtp] = useState("");
+
 	const router = useRouter();
 
 	const supabase = createClient();
@@ -46,24 +45,38 @@ const LoginPage = () => {
 		register,
 		handleSubmit,
 		formState: { errors },
-		// reset,
-	} = useForm<{ email: string; password: string }>();
+	} = useForm<{ email: string; password?: string }>();
+
 	const onSubmit: SubmitHandler<{
 		email: string;
-		password: string;
+		password?: string;
 	}> = async (data) => {
 		setIsLoading(true);
-		const { error } = await signInWithEmailPassword(
-			data?.email,
-			data?.password,
-			supabase,
-		);
+		const { error } = await signInWithOtp(data?.email, supabase);
 		setIsLoading(false);
 
 		if (error) {
 			errorToast(error?.message);
 		} else {
-			void router?.push(`/${ALL_BOOKMARKS_URL}`);
+			setEmail(data.email);
+			setIsOtpStep(true);
+		}
+	};
+
+	const handleVerifyOtp = async () => {
+		if (!otp || !email) {
+			errorToast("Please enter OTP");
+			return;
+		}
+
+		setIsLoading(true);
+		const { error } = await verifyOtp(email, otp, supabase);
+		setIsLoading(false);
+
+		if (error) {
+			errorToast(error.message);
+		} else {
+			void router.push(`/${ALL_BOOKMARKS_URL}`);
 		}
 	};
 
@@ -78,73 +91,73 @@ const LoginPage = () => {
 							</figure>
 							<p>recollect</p>
 						</div>
-						<form
-							className="flex flex-col items-center justify-center space-y-4"
-							onSubmit={handleSubmit(onSubmit)}
-						>
-							<Input
-								{...register("email", {
-									required: {
-										value: true,
-										message: "Please enter email",
-									},
-									pattern: {
-										value: EMAIL_CHECK_PATTERN,
-										message: "Please enter valid email",
-									},
-								})}
-								className={grayInputClassName}
-								errorText={errors?.email?.message ?? ""}
-								id="email"
-								isError={Boolean(errors?.email)}
-								placeholder="Email"
-							/>
-							<Input
-								{...register("password", {
-									required: {
-										value: true,
-										message: "Please enter password",
-									},
-								})}
-								className={grayInputClassName}
-								errorText={errors?.password?.message ?? ""}
-								id="password"
-								isError={Boolean(errors?.password)}
-								placeholder="Password"
-								type="password"
-							/>
-							<button
-								className={buttonDarkClassName}
-								id="sign-in-button"
-								type="submit"
+						{!isOtpStep ? (
+							<form
+								className="flex flex-col items-center justify-center space-y-4"
+								onSubmit={handleSubmit(onSubmit)}
 							>
-								{!isLoading ? "Sign in" : <Spinner />}
-							</button>
-							<div
-								className={buttonLightClassName}
-								onClick={() => {
-									(async () => {
-										await signInWithOauth("google", supabase);
-									})();
-								}}
-								onKeyDown={() => {}}
-								role="button"
-								tabIndex={0}
-							>
-								<figure className="mr-[6px]">
-									<GoogleLoginIcon />
-								</figure>
-								<p>Continue with Google</p>
+								<Input
+									{...register("email", {
+										required: {
+											value: true,
+											message: "Please enter email",
+										},
+										pattern: {
+											value: EMAIL_CHECK_PATTERN,
+											message: "Please enter valid email",
+										},
+									})}
+									className={grayInputClassName}
+									errorText={errors?.email?.message ?? ""}
+									id="email"
+									isError={Boolean(errors?.email)}
+									onChange={(event) => setEmail(event.target.value)}
+									placeholder="Email"
+								/>
+								<button
+									className={buttonDarkClassName}
+									id="sign-in-button"
+									type="submit"
+								>
+									{!isLoading ? "Login" : <Spinner />}
+								</button>
+								<div
+									className={buttonLightClassName}
+									onClick={() => {
+										(async () => {
+											await signInWithOauth("google", supabase);
+										})();
+									}}
+									onKeyDown={() => {}}
+									role="button"
+									tabIndex={0}
+								>
+									<figure className="mr-[6px]">
+										<GoogleLoginIcon />
+									</figure>
+									<p>Continue with Google</p>
+								</div>
+							</form>
+						) : (
+							<div className="flex flex-col items-center justify-center space-y-4">
+								<Input
+									className={grayInputClassName}
+									errorText=""
+									isError={false}
+									onChange={(event) => setOtp(event.target.value)}
+									placeholder="Enter OTP"
+									value={otp}
+								/>
+								<button
+									className={buttonDarkClassName}
+									disabled={otp.length !== 6}
+									onClick={handleVerifyOtp}
+									type="submit"
+								>
+									{isLoading ? <Spinner /> : "Verify OTP"}
+								</button>
 							</div>
-						</form>
-					</div>
-				</div>
-				<div className="fixed bottom-0 flex w-full items-center justify-center py-5">
-					<div className="flex w-[300px] items-center justify-between">
-						<p className={bottomBarText}>Don’t have an account?</p>
-						<a className={bottomBarButton} href={`/${SIGNUP_URL}`}>
-							Sign up
-						</a>
+						)}
 					</div>
 				</div>
 			</div>
