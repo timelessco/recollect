be7dc179307b64e1577b35832eea0106b98504f6
B-a-l-aj-i <balajivijay679@gmail.com>
2025-11-21 12:34:36 +0530

    refactor: remove sentry for no data

--- src/pages/api/bookmark/add-url-screenshot.ts
+++ src/pages/api/bookmark/add-url-screenshot.ts
@@ -269,16 +269,7 @@ export default async function handler(
 		}
 
 		if (!data || data.length === 0) {
-			console.warn("No data returned from the database");
-			Sentry.captureException(new Error("No data returned from the database"), {
-				tags: {
-					operation: "update_bookmark_screenshot",
-					userId,
-				},
-				extra: {
-					bookmarkId: request.body.id,
-				},
-			});
+			console.log("No data returned from the database");
 			response.status(500).json({
 				data: null,
 				error: "No data returned from the database",
