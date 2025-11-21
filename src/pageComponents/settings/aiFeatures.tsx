0f3227ddae8c3d08c821c132ad1669a57858aaa0
widejoy <rogerantony40@gmail.com>
2025-11-21 14:59:52 +0530

    fix(password-show): :bug: now value in api key field would never be undefined

--- src/pageComponents/settings/aiFeatures.tsx
+++ src/pageComponents/settings/aiFeatures.tsx
@@ -117,7 +117,7 @@ export const AiFeatures = () => {
 									? showKey
 										? apiKey || ""
 										: "••••••••••••••••••••••••••••••••"
-									: undefined
+									: ""
 							}
 							placeholder={
 								isSaving
