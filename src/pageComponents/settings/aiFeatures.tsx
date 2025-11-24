8f5f45d13bd24da468abaa456f32420f8675b0cb
widejoy <rogerantony40@gmail.com>
2025-11-24 12:02:11 +0530

    revert(icons): :rewind: reverting changes to match with the repo

--- src/pageComponents/settings/aiFeatures.tsx
+++ src/pageComponents/settings/aiFeatures.tsx
@@ -16,7 +16,7 @@ import {
 	settingsInputContainerClassName,
 } from "../../utils/commonClassNames";
 
-import { ShowEyeIcon } from "@/icons/show-eye";
+import { ShowEyeIcon } from "@/icons/show-eye-icon";
 import { SlashedEyeIcon } from "@/icons/slashed-eye-icon";
 import { handleClientError } from "@/utils/error-utils/client";
 
