ad0a258e11547348e54b2127d35d40b76f41b8e4
widejoy <rogerantony40@gmail.com>
2025-11-28 17:13:24 +0530

    refactor(useIsMobileView): change export to named function for improved clarity

--- src/hooks/useIsMobileView.ts
+++ src/hooks/useIsMobileView.ts
@@ -1,7 +1,7 @@
 import { useMediaQuery } from "@react-hookz/web";
 
 // tells if the screen is in mobile view or not
-export default function useIsMobileView() {
+export function useIsMobileView() {
 	const isMobile = useMediaQuery("(max-width: 600px)");
 	const isTablet = useMediaQuery("(min-width: 601px) and (max-width: 1023px)");
 	const isDesktop = !isMobile && !isTablet;
