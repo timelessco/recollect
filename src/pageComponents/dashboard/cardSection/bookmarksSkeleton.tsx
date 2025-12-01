b1d7e9c31aabb4ffb0ff50f0207ffe4415c398c8
rogerantony <101205954+rogerantony-dev@users.noreply.github.com>
2025-12-01 14:00:23 +0530

    feat(rectwebhookz): remove react responsive and use reacthookzweb (#639)
    * refactor(useIsMobileView): update media query imports and syntax for better responsiveness
    
    * chore(dependencies): remove react-responsive from package.json and pnpm-lock.yaml
    
    * refactor(useIsMobileView): change export to named function for improved clarity
    
    * refactor: update import statements for useIsMobileView to named imports across multiple components
    
    * refactor(bookmarksSkeleton): update import statement for useIsMobileView to named import
    
    * fix(bookmarksSkeleton): disable eslint rule for array index keys to prevent warnings

--- src/pageComponents/dashboard/cardSection/bookmarksSkeleton.tsx
+++ src/pageComponents/dashboard/cardSection/bookmarksSkeleton.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import useIsMobileView from "../../../hooks/useIsMobileView";
+import { useIsMobileView } from "../../../hooks/useIsMobileView";
 import { viewValues } from "../../../utils/constants";
 import { getColumnCount } from "../../../utils/helpers";
 
