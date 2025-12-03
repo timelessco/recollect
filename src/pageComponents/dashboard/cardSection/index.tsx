501d4c70f23f46163f8bae7fe6f57bd0618e91e3
widejoy <rogerantony40@gmail.com>
2025-12-03 11:55:11 +0530

    fix(dashboard): update DefaultUserIcon visibility on hover in CardSection component

--- src/pageComponents/dashboard/cardSection/index.tsx
+++ src/pageComponents/dashboard/cardSection/index.tsx
@@ -464,7 +464,11 @@ const CardSection = ({
 			);
 		}
 
-		return <DefaultUserIcon className={`h-5 w-5 ${avatarClassName}`} />;
+		return (
+			<DefaultUserIcon
+				className={`hidden h-5 w-5 group-hover:flex ${avatarClassName}`}
+			/>
+		);
 	};
 
 	const renderUrl = (item: SingleListData) => (
