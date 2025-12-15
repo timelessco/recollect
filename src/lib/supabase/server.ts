1692253b5524830aa18a8d45b275907a767b6a81
Navin Moorthy <navin007.a@gmail.com>
2025-12-15 13:11:18 +0530

    refactor(auth): ♻️ update Supabase client initialization in authentication routes
    - Replace `createClient` with `createServerClient`
    - Refactor `createClient` function to `createServerClient`

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,5 +1,5 @@
 import { cookies } from "next/headers";
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient as createSupabaseServerClient } from "@supabase/ssr";
 
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "./constants";
 import { type Database } from "@/types/database.types";
@@ -8,10 +8,10 @@ import { type Database } from "@/types/database.types";
  * If using Fluid compute: Don't put this client in a global variable. Always create a new client within each
  * function when using it.
  */
-export async function createClient() {
+export async function createServerClient() {
 	const cookieStore = await cookies();
 
-	return createServerClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+	return createSupabaseServerClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
 		cookies: {
 			getAll() {
 				return cookieStore.getAll();
