f8238b2e88a0524f7df72d1f278f2c80d07961ae
rogerantony-dev <rogerantony40@gmail.com>
2026-02-06 17:21:59 +0530

    refactor(hash-icon): simplify component structure for improved readability

--- src/icons/hash-icon.tsx
+++ src/icons/hash-icon.tsx
@@ -1,15 +1,13 @@
 import { Icon, type IconProps } from "@/components/ui/recollect/icon";
 
-export const HashIcon = (props: IconProps) => {
-	return (
-		<Icon fill="none" viewBox="0 0 14 14" {...props}>
-			<path
-				stroke="currentColor"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth=".875"
-				d="m5.542 1.75-1.75 10.5m6.417-10.5-1.75 10.5M11.958 4.666H2.041m9.334 4.667H1.458"
-			/>
-		</Icon>
-	);
-};
+export const HashIcon = (props: IconProps) => (
+	<Icon fill="none" viewBox="0 0 14 14" {...props}>
+		<path
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth=".875"
+			d="m5.542 1.75-1.75 10.5m6.417-10.5-1.75 10.5M11.958 4.666H2.041m9.334 4.667H1.458"
+		/>
+	</Icon>
+);
