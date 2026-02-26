066cf78d4c408255acca9ee97863dabda92fe0e9
rogerantony <101205954+rogerantony-dev@users.noreply.github.com>
2026-02-26 11:59:49 +0530

    refactor(components): replace AriaDisclosure with Collapsible component (#807)

---
+++ src/components/ui/recollect/collapsible.tsx
@@ -0,0 +1,37 @@
+import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible";
+
+import { cn } from "@/utils/tailwind-merge";
+
+function Root(props: CollapsiblePrimitive.Root.Props) {
+	const { className, ...rest } = props;
+
+	return (
+		<CollapsiblePrimitive.Root
+			defaultOpen
+			className={cn("contents", className)}
+			{...rest}
+		/>
+	);
+}
+
+function Trigger(props: CollapsiblePrimitive.Trigger.Props) {
+	const { className, ...rest } = props;
+
+	return (
+		<CollapsiblePrimitive.Trigger
+			className={cn("aria-disclosure-button w-full cursor-pointer", className)}
+			type="button"
+			{...rest}
+		/>
+	);
+}
+
+function Panel(props: CollapsiblePrimitive.Panel.Props) {
+	return <CollapsiblePrimitive.Panel keepMounted {...props} />;
+}
+
+export const Collapsible = {
+	Root,
+	Trigger,
+	Panel,
+};
