2c03e38eabeff752e1b9b4921bb69bcb5878b400
rogerantony-dev <rogerantony40@gmail.com>
2026-03-03 12:42:59 +0530

    feat(bookmarksSortDropdown): add sorting functionality with new SortItemContent component

--- src/components/customDropdowns.tsx/bookmarksSortDropdown.tsx
+++ src/components/customDropdowns.tsx/bookmarksSortDropdown.tsx
@@ -53,8 +53,44 @@ function BookmarksSortButton() {
 			<p className="ml-[6px]">{currentValue?.label}</p>
 		</div>
 	);
+}
 
-	const selectItemContent = (value: string) => (
+function BookmarksSortItems() {
+	const { sortBy } = useGetSortBy();
+	const { setBookmarksView } = useBookmarksViewUpdate();
+
+	return (
+		<div>
+			<div className="px-2 py-[6px] text-[12px] leading-[115%] font-450 tracking-[0.02em] text-gray-600">
+				Sort by
+			</div>
+			{sortOptions.map((item) => (
+				<Button
+					className={`w-full text-left ${dropdownMenuItemClassName}`}
+					key={item.value}
+					onClick={() =>
+						setBookmarksView(item.value as BookmarksSortByTypes, "sort")
+					}
+				>
+					<SortItemContent
+						icon={item.icon}
+						isSelected={item.value === sortBy}
+						label={item.label}
+					/>
+				</Button>
+			))}
+		</div>
+	);
+}
+
+interface SortItemContentProps {
+	icon: React.ReactNode;
+	isSelected: boolean;
+	label: string;
+}
+
+function SortItemContent({ icon, isSelected, label }: SortItemContentProps) {
+	return (
 		<div className="flex items-center py-px">
 			<figure className="mr-[6px] h-4 w-4">{icon}</figure>
 			<div className="flex w-full items-center justify-between">
