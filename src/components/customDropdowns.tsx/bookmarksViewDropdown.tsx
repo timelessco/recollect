f883760a1059a3af95e017f6f70dfef0b393d50a
rogerantony-dev <rogerantony40@gmail.com>
2026-03-04 12:09:13 +0530

    refactor(slider): remove old slider component and integrate new bookmarks view slider

--- src/components/customDropdowns.tsx/bookmarksViewDropdown.tsx
+++ src/components/customDropdowns.tsx/bookmarksViewDropdown.tsx
@@ -1,7 +1,6 @@
-import { useCallback, useRef } from "react";
+import { useRef } from "react";
 import { Popover } from "@base-ui/react/popover";
 import { Bars4Icon } from "@heroicons/react/20/solid";
-import debounce from "lodash/debounce";
 import find from "lodash/find";
 
 import { useBookmarksViewUpdate } from "../../hooks/useBookmarksViewUpdate";
@@ -13,9 +12,9 @@ import { type BookmarksViewTypes } from "../../types/componentStoreTypes";
 import { dropdownMenuItemClassName } from "../../utils/commonClassNames";
 import { singleInfoValues, viewValues } from "../../utils/constants";
 import RadioGroup from "../radioGroup";
-import Slider from "../slider";
 
 import { BookmarkCardContentSwitch } from "./bookmark-card-content-switch";
+import { BookmarksViewSlider } from "./bookmarks-view-slider";
 
 type BookmarksViewDropdownProps = {
 	// based on this it is either rendered in dropdown or in the sliding menu component if its in responsive mobile page
@@ -29,7 +28,6 @@ export const BookmarksViewDropdown = (props: BookmarksViewDropdownProps) => {
 
 	const { setBookmarksView } = useBookmarksViewUpdate();
 
-	const bookmarksColumns = useGetViewValue("moodboardColumns", [10]);
 	const bookmarksViewValue = useGetViewValue("bookmarksView", "");
 
 	type CardContentOptionsTypes = {
@@ -89,12 +87,6 @@ export const BookmarksViewDropdown = (props: BookmarksViewDropdownProps) => {
 		</div>
 	);
 
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	const setColumnsCallback = useCallback(
-		debounce((value) => setBookmarksView(value as number[], "columns"), 200),
-		[setBookmarksView],
-	);
-
 	const dropdownContent = (
 		<>
 			{renderDropdownHeader("View as")}
@@ -120,20 +112,7 @@ export const BookmarksViewDropdown = (props: BookmarksViewDropdownProps) => {
 						Columns
 					</p>
 					<div className="mt-px w-[90px]">
-						<Slider
-							defaultValue={bookmarksColumns as unknown as number}
-							label="moodboard-cols-slider"
-							maxValue={50}
-							minValue={10}
-							onChangeEnd={(value) => {
-								const columValue = value as number[];
-								// do not fire api if the new value is the same as previous value
-								if (columValue?.[0] !== bookmarksColumns?.[0]) {
-									setColumnsCallback(value);
-								}
-							}}
-							step={10}
-						/>
+						<BookmarksViewSlider />
 					</div>
 				</div>
 			)}
